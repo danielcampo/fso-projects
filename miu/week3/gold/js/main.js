76ea44f70618b7207ac6898c5cb3acbc08ef882e
Daniel Campo <accounts@danielcampo.com>
2013-02-22 00:19:19 -0500

    Bug fixes and libs.

--- miu/week3/gold/js/main.js
+++ miu/week3/gold/js/main.js
@@ -9,23 +9,21 @@ App Description: Keep track of your extensive game library.
 
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
-});	
-		
+});
+
 $('#add').on('pageinit', function(){
-		/*
 		var myForm = $('#gameForm');
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
 			},
 			submitHandler: function() {
 				var data = myForm.serializeArray();
-					storeData(data);
+					saveGame(data);
 				}
 			});
-		*/
-	
+
 	//any other code needed for addItem page goes here.
-	
+
 	/* ################################ */
 	/* ################################ */
 	/* Form Building Functions */
@@ -41,42 +39,42 @@ $('#add').on('pageinit', function(){
 		["platform_wii", "Wii / Wii U"],
 		["platform_xbox360", "XBOX 360"]
 	];
-	
+
 	function createPlatformList() {
 
 		// Label
 		var platformLabel = $("<label></label>");
 			platformLabel.attr("for","platform");
-		
-		var platformLabelText = "Platform ";	
+
+		var platformLabelText = "Platform ";
 		platformLabel.append(platformLabelText);
-		
+
 		$("#platform_list").append(platformLabel);
-		
+
 		// Select
 		var platformSelect = $("<select></select>");
 			platformSelect.attr("id","platform");
 			platformSelect.attr("name","platform");
-		
+
 		for (n = 0; n < platformList.length; n++) {
 			createOption = $("<option></option>");
 				createOption.attr("value", platformList[n][0]);
-			
+
 			// Option Text
 			optionText = platformList[n][1];
-			
+
 			// Append Option Text
 			createOption.append(optionText);
-			
+
 			platformSelect.append(createOption);
-			
+
 		};
-		
+
 		// Append Select to #platform
-		$("#platform_list").append(platformSelect);	
+		$("#platform_list").append(platformSelect);
 
 	};
-	
+
 	createPlatformList();
 	// END Platform List
 
@@ -93,48 +91,48 @@ $('#add').on('pageinit', function(){
 		["genre_simulation","Simulation"],
 		["genre_sports","Sports"],
 		["genre_strategy","Strategy"]
-	];	
-	
+	];
+
 	function createGenreList() {
-			
+
 		// Label
 		var genreLabel = $("<label></label>");
 			genreLabel.attr("for","genre");
-		
-		var genreLabelText = "Genre ";	
+
+		var genreLabelText = "Genre ";
 		genreLabel.append(genreLabelText);
-		
+
 		$("#genre_list").append(genreLabel);
-		
+
 		// Select
 		var genreSelect = $("<select></select>");
-			genreSelect.id = "genre";
-			genreSelect.name = "genre";
-		
+			genreSelect.attr("id","genre");
+			genreSelect.attr("name","genre");
+
 		for (n = 0; n < genreList.length; n++) {
 			createOption = $("<option></option>");
 				createOption.attr("value", genreList[n][0]);
-			
+
 			// Option Text
 			optionText = genreList[n][1];
-			
+
 			// Append Option Text
 			createOption.append(optionText);
-			
+
 			genreSelect.append(createOption);
-			
+
 		};
-		
+
 		// Append Select to #genre
 		$("#genre_list").append(genreSelect);
 	};
 	createGenreList();
 	// END Genre List
-	
+
 	/* ################################ */
 	/* END Form Building Functions
 	/* ################################ */
-	
+
 	function getOptionText(option,value) {
 		// Cycles through the Genre list array to find a match using the value of the selected option
 		for (n = 0; n < option.length; n++) {
@@ -144,7 +142,7 @@ $('#add').on('pageinit', function(){
 			};
 		};
 	};
-	
+
 	function getOptionValue(option,value) {
 	// Cycles through the Genre list array to find a match using the value of the selected option
 		for (n = 0; n < option.length; n++) {
@@ -154,7 +152,7 @@ $('#add').on('pageinit', function(){
 			};
 		};
 	};
-	
+
 	function getOptionValueIndex(option,value) {
 	// Cycles through the Genre list array to find a match using the value of the selected option
 		for (n = 0; n < option.length; n++) {
@@ -163,13 +161,13 @@ $('#add').on('pageinit', function(){
 			};
 		};
 	};
-	
+
 	// ##################################
 	// ##################################
 	// Event Listeners
 	// ##################################
 	// ##################################
-	
+
 		/* Display Games */
 		$("#display_games").click(displayGames);
 
@@ -177,45 +175,45 @@ $('#add').on('pageinit', function(){
 		$("#clear_games").click(deleteGames);
 
 		/* Save Game */
-		$("#submit").click(saveGame);
-		
+
 	// ##################################
 	// Event Listeners
 	// ##################################
 
-	
+
 		var autofillData = function (){
-			 
+
 		};
-		
+
 		// ####################################################################
 		// Create
 		// ####################################################################
-		
+
 		// ##################################
 		// ##################################
 		// Save Game to Local Storage
 		// ##################################
 		// ##################################
-		function saveGame(key) {
-			if (!key) {
+		function saveGame(data) {
+			//if (!key) {
 				var id = Math.round(new Date().getTime() / 1000);
-			} else {
+			/* } else {
 				var id = key;
 			};
+			*/
 			// Store Form Fields Value Into an Object
 			// Label & Value Will Be Stored.
 
 			var game = {};
 
-				game.title = ["Title", $("#title").value];
+				game.title = ["Title", $("#title").val()];
 
-				game.platform = ["Platform", getOptionText(platformList,$("#platform").value)];
+				game.platform = ["Platform", getOptionText(platformList,$("#platform").val())];
 
-				game.genre = ["Genre", getOptionText(genreList,$("#genre").value)];
+				game.genre = ["Genre", getOptionText(genreList,$("#genre").val())];
 
-				game.publisher = ["Publisher", $("#publisher").value];
-				game.developer = ["Developer", $("#developer").value];
+				game.publisher = ["Publisher", $("#publisher").val()];
+				game.developer = ["Developer", $("#developer").val()];
 
 				if ($("#completed_yes").checked) {
 					game.completed = ["Completed", "Yes"];
@@ -224,16 +222,16 @@ $('#add').on('pageinit', function(){
 				};
 
 				// Purchased
-				game.purchased = ["Purchased", $("#purchased").value];
-				game.purchased_amount = ["Purchase Amount", $("#purchased_amount").value];
+				game.purchased = ["Purchased", $("#purchased").val()];
+				game.purchased_amount = ["Purchase Amount", $("#purchased_amount").val()];
 
 				// Sold
-				game.sold = ["Sold", $("#sold").value];
-				game.sold_amount = ["Sold Amount", $("#sold_amount").value];
+				game.sold = ["Sold", $("#sold").val()];
+				game.sold_amount = ["Sold Amount", $("#sold_amount").val()];
 
 
 				// Special Notes
-				game.notes = ["Special Notes", $("#notes").value];
+				game.notes = ["Special Notes", $("#notes").val()];
 
 				// Favorite
 				if ($("#favorite").checked) {
@@ -244,8 +242,10 @@ $('#add').on('pageinit', function(){
 
 			// Save Date into Local Storage: Stringify converts Coupon object to a string for local storage capability.
 			localStorage.setItem(id, JSON.stringify(game));
-			
+
 			alert("Game Has Been Saved!");
+
+			window.location.reload();
 
 		};
 		// ##################################
@@ -255,12 +255,12 @@ $('#add').on('pageinit', function(){
 		// ####################################################################
 		// END Create
 		// ####################################################################
-		
+
 
 		// ####################################################################
 		// Read
 		// ####################################################################
-		
+
 		// ##################################
 		// ##################################
 		// Display Game(s) From Local Storage
@@ -347,7 +347,7 @@ $('#add').on('pageinit', function(){
 		// Make edit coupon link list
 		// ##################################
 		// ##################################
-		
+
 		// Create the edit and delete links for each store coupon when displayed
 		function makeEditList(key, editGameListRow) {
 
@@ -390,17 +390,17 @@ $('#add').on('pageinit', function(){
 		// ##################################
 		// Make edit coupon link list
 		// ##################################
-		
-		
+
+
 		// ####################################################################
 		// END Read
 		// ####################################################################
-		
-		
+
+
 		// ####################################################################
 		// Update
 		// ####################################################################
-		
+
 		// ##################################
 		// ##################################
 		// Edit saved game
@@ -416,18 +416,18 @@ $('#add').on('pageinit', function(){
 
 			// populate form fields with data from local storage
 			$("#title").val(game.title[1]);
-			
+
 			$("#platform").selectedIndex = getOptionValueIndex(platformList,game.platform[1]);
 
 			$("#genre").selectedIndex = getOptionValueIndex(genreList,game.genre[1]);
 
 			$("#publisher").val(game.publisher[1]);
 			$("#developer").val(game.developer[1]);
-	
+
 			if (game.completed[1] === "Yes") {
-				$("#completed_yes").attr("checked","checked");
+				$("#completed_yes").attr("checked",true).checkboxradio("refresh");
 			} else {
-				$("#completed_no").attr("checked","checked");
+				$("#completed_no").attr("checked",true).checkboxradio("refresh");
 			};
 
 			// Purchased
@@ -436,8 +436,11 @@ $('#add').on('pageinit', function(){
 
 
 			// Sold
-			$("#sold").value = game.sold[1];
+			$("#sold").val(game.sold[1]);
+
 			if (game.sold[1] != "n/a") {
+				$("#sold_q").attr("checked",true).checkboxradio("refresh");
+				$("#sold_d").css("display","block");
 				$("#sold_amount").val(game.sold[1]);
 			};
 
@@ -447,17 +450,16 @@ $('#add').on('pageinit', function(){
 
 			// Special Notes
 			$("#notes").val(game.notes[1]);
-			
+
 			// Favorite
 			if (game.favorite[1] === "Yes") {
 				$("#favorite").attr("checked","true");
 			};
-			
-			$("#submit").val("[#] Edit Game");
-			
-			var editAdd = $("#submit");
-			
-			editAdd.click(function() {
+
+			var editSubmit = $("#submit")
+				editSubmit.val("[#] Edit Game");
+
+			$("#submit").click(function() {
 				saveEditedGame(key);
 			});
 
@@ -465,23 +467,23 @@ $('#add').on('pageinit', function(){
 		// ##################################
 		// Edit saved game
 		// ##################################
-		
-		
+
+
 		// ##################################
 		// Save Edited Game
 		// ##################################
-		function saveEditedGame(e) {
-			saveGame(this.key);
+		function saveEditedGame(key) {
+			saveGame(key);
 		};
 		// ##################################
 		// END Save Edited Game
 		// ##################################
-		
+
 		// ####################################################################
 		// END Update
 		// ####################################################################
-		
-		
+
+
 		// ####################################################################
 		// Delete
 		// ####################################################################
@@ -507,10 +509,10 @@ $('#add').on('pageinit', function(){
 		// ##################################
 		// Delete saved game
 		// ##################################
-			
 
 
-			
+
+
 		// ##################################
 		// ##################################
 		// Deleted ALL Game(s) From Local Storage
@@ -529,19 +531,31 @@ $('#add').on('pageinit', function(){
 		};
 		// ##################################
 		// Deleted ALL Game(s) From Local Storage
-		// ##################################	
+		// ##################################
 
 		// ####################################################################
 		// END Delete
 		// ####################################################################
-		
-		
-		
-		
+
+
+
+
 		//
 		// Utiliy Functions
 		//
-		
+
+		// ##################################
+		// ##################################
+		// Cancel
+		// ##################################
+		// ##################################
+		$("#cancel a").click(function() {
+			window.location.reload();
+		});
+		// ##################################
+		// END Cancel
+		// ##################################
+
 		// ##################################
 		// ##################################
 		// Add Example Coupon Data
@@ -559,8 +573,8 @@ $('#add').on('pageinit', function(){
 		// ##################################
 		// Add Example Coupon Data
 		// ##################################
-		
-		
+
+
 		// ##################################
 		// ##################################
 		// Hide form and relevant controls when games are displayed
@@ -588,7 +602,21 @@ $('#add').on('pageinit', function(){
 		};
 		// ##################################
 		// Hide form and relevant controls when games are displayed
-		// ##################################	
-	
+		// ##################################
+
+		function hideConditionals() {
+			$("#sold_d").hide();
+		};
+		hideConditionals();
+
+		$("#sold_q").change(function() {
+
+			if ($(this).attr("checked")) {
+				$("#sold_d").slideDown();
+			} else {
+				$("#sold_d").slideUp();
+			}
+		});
+
 });
 
